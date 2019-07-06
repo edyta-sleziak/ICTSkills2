6cce355886f99a76ece40c653657c8f864d0aca0
LAPTOP-QQULPCJ7\edyta <edyta.sleziak@gmail.com>
2019-07-06 14:23:40 +0100

    Exercise 3 completed

--- components/exercises/03_props.js
+++ components/exercises/03_props.js
@@ -1,10 +1,34 @@
 import React , { Component } from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
-export default class CourseModulesWithProps extends Component { 
+export default class CourseModulesWithProps extends Component {
     render() {
+        let header = "HDip Computer Science - Modules table";
         return (
-            <h1> TODO </h1> 
-          );
+            <div>
+                <h1>{`${header}`}</h1>
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>No lectures</th>
+                        <th>No practicals</th>
+                    </tr>
+                    </thead>
+                    <tbody >
+                    <tr>
+                        <td>{this.props.modules[0].name}</td>
+                        <td>{this.props.modules[0].noLectures}</td>
+                        <td>{this.props.modules[0].noPracticals}</td>
+                    </tr>
+                    <tr>
+                        <td>{this.props.modules[1].name}</td>
+                        <td>{this.props.modules[1].noLectures}</td>
+                        <td>{this.props.modules[1].noPracticals}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 }
